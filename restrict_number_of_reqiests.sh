730600edbf2d05fb3d632071c192ddac86262a41
PyAutoML <147545771+pyautoml@users.noreply.github.com>
2023-11-24 19:21:17 +0100

    Create restrict_number_of_reqiests.sh
    Restrict the number of requests per second from users.

---
+++ restrict_number_of_reqiests.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Ban users who send more than N requests per minute.
+
+LOG_FILE="" # specify log file, e.g. "/var/log/nginx/access.log"
+RATE_LIMIT=5
+
+# Extract unique IP addresses and count their occurrences per second
+IP_COUNTS=$(awk '{print $1}' "$LOG_FILE" | sort | uniq -c | sort -nr)
+
+while read -r COUNT IP; do
+    if [ "$COUNT" -gt "$RATE_LIMIT" ]; then
+        ufw deny from "$IP" to any
+        echo "Blocked IP: $IP"
+    fi
+done <<< "$IP_COUNTS"
+
+
+
+
+
